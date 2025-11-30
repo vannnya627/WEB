59551d974b3935c4a2b4193d6748b6c54db18490
vanya <Sumskiyivan627@gmail.com>
2025-12-01 00:07:49 +0200

    practice6

---
+++ practice6/index..php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Практична робота №6</title>
+    <link rel="stylesheet" href="css/practice6.css">
+</head>
+<body>
+
+<div class="scene">
+    <div class="text-overlay">Практична робота №6</div>
+
+    <div class="sun"></div>
+    <div class="cloud"></div>
+    <div class="cloud"></div>
+    <div class="mountains-far"></div>
+    <div class="mountains"></div>
+    <div class="forest"></div>
+    <div class="ground"></div>
+    <div class="dragon"></div>
+</div>
+
+</body>
+</html>
